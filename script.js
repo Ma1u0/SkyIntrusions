696983b82378da15551d2c30e6e62a8b88ffa623
Matu <90573043+Ma1u0@users.noreply.github.com>
2025-12-13 15:59:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a simple test marker
-L.marker([55.6761, 12.5683])
-  .addTo(map)
-  .bindPopup("Copenhagen – Test Marker");
+/* =========================
+   ICON FACTORY
+========================= */
+function createIcon(file) {
+    return L.icon({
+        iconUrl: `icons/${file}`,
+        iconSize: [36, 36],
+        iconAnchor: [18, 36],
+        popupAnchor: [0, -30]
+    });
+}
 
 /* =========================
    UAV ICONS
