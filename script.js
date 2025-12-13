5c47ac97d501696d52214ca629b96f0c27f67e49
Matu <90573043+Ma1u0@users.noreply.github.com>
2025-12-13 15:55:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([55.6761, 12.5683])
   .addTo(map)
   .bindPopup("Copenhagen – Test Marker");
+
+/* =========================
+   UAV ICONS
+========================= */
+const uavRed = createIcon("uav-red.png");
+const uavOrange = createIcon("uav-orange.png");
+const uavYellow = createIcon("uav-yellow.png");
+const uavGreen = createIcon("uav-green.png");
+const uavBlue = createIcon("uav-blue.png");
+
+/* =========================
+   JET ICONS
+========================= */
+const jetRed = createIcon("jet-red.png");
+const jetOrange = createIcon("jet-orange.png");
+const jetYellow = createIcon("jet-yellow.png");
+const jetGreen = createIcon("jet-green.png");
+const jetBlue = createIcon("jet-blue.png");
+
+/* =========================
+   BALLOON ICONS
+========================= */
+const balloonRed = createIcon("balloon-red.png");
+const balloonOrange = createIcon("balloon-orange.png");
+const balloonYellow = createIcon("balloon-yellow.png");
+const balloonGreen = createIcon("balloon-green.png");
+const balloonBlue = createIcon("balloon-blue.png");
+
+// Example marker with UAV red
+L.marker([55.6761, 12.5683], { icon: uavRed })
+  .addTo(map)
+  .bindPopup("Copenhagen – UAV Red Test");
